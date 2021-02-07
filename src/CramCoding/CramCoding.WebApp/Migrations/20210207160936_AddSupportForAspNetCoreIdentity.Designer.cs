53157b02968d2fa7a39e4b5b06441a12fa2987f7
michalantolik <michal.antolik@gmail.com>
2021-02-07 17:16:27 +0100

    Add DB migration to support ASP.NET Core Identity

--- src/CramCoding/CramCoding.WebApp/Migrations/20210207160936_AddSupportForAspNetCoreIdentity.Designer.cs
+++ src/CramCoding/CramCoding.WebApp/Migrations/20210207160936_AddSupportForAspNetCoreIdentity.Designer.cs
@@ -4,14 +4,16 @@ using CramCoding.WebApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CramCoding.WebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210207160936_AddSupportForAspNetCoreIdentity")]
+    partial class AddSupportForAspNetCoreIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

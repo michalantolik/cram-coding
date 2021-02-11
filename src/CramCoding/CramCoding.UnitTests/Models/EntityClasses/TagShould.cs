22bf9735351f19c430d5d30bf036c0f519ab2325
michalantolik <michal.antolik@gmail.com>
2021-02-11 14:40:19 +0100

    Add unit tests for Tag
    - Collection navigation properties initialization tested

---
+++ src/CramCoding/CramCoding.UnitTests/Models/EntityClasses/TagShould.cs
@@ -0,0 +1,16 @@
+﻿using CramCoding.WebApp.Models.EntityClasses;
+using Xunit;
+
+namespace CramCoding.UnitTests.Models.EntityClasses
+{
+    public class TagShould
+    {
+        [Fact]
+        public void InitializePostsCollection()
+        {
+            var sut = new Tag();
+            Assert.NotNull(sut.Posts);
+            Assert.Empty(sut.Posts);
+        }
+    }
+}

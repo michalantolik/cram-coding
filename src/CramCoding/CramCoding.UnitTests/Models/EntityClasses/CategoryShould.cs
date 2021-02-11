0c9bdee6821852aae62e31ef759715e737f01879
michalantolik <michal.antolik@gmail.com>
2021-02-11 14:35:14 +0100

    Add unit tests for Category
    - Collection navigation properties initialization tested

---
+++ src/CramCoding/CramCoding.UnitTests/Models/EntityClasses/CategoryShould.cs
@@ -0,0 +1,16 @@
+﻿using CramCoding.WebApp.Models.EntityClasses;
+using Xunit;
+
+namespace CramCoding.UnitTests.Models.EntityClasses
+{
+    public class CategoryShould
+    {
+        [Fact]
+        public void InitializePostsCollection()
+        {
+            var sut = new Category();
+            Assert.NotNull(sut.Posts);
+            Assert.Empty(sut.Posts);
+        }
+    }
+}

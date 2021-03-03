b871b9838ca256867c85fa3d32a4141f0b42bba2
michalantolik <michal.antolik@gmail.com>
2021-03-03 15:37:32 +0100

    Add ActiveAnchorTagHelper
    - Presence of "active" class in anchor tag is controlled via tag helper
    - Anchor is "active" when anchor text equals "active-item" tag parameter

---
+++ src/CramCoding/CramCoding.UnitTests/TagHelpers/ActiveAnchorTagHelperShould.cs
@@ -0,0 +1,228 @@
+﻿using CramCoding.WebApp.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CramCoding.UnitTests.TagHelpers
+{
+    public class ActiveAnchorTagHelperShould
+    {
+        #region Test methods
+
+        [Fact]
+        public async void ThrowExceptionWhenNullContext()
+        {
+            // ARRANGE
+            var sut = new ActiveAnchorTagHelper();
+
+            var outputAttributes = CreateAttributeList(("class", "bg-dark"), ("href", "#"));
+            var childContentFactory = CreateChildContentFactory("AnchorText 123");
+            TagHelperOutput output = new TagHelperOutput("a", outputAttributes, childContentFactory);
+
+            // ACT & ASSERT
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.ProcessAsync(context: null, output));
+        }
+
+        [Fact]
+        public async void ThrowExceptionWhensNullOutput()
+        {
+            // ARRANGE
+            var sut = new ActiveAnchorTagHelper();
+
+            var contextAttributes = CreateAttributeList(("href", "#"), ("active-item", "AnchorText 123"));
+            var emptyContextItems = CreateEmptyContextItems();
+            var uniqueId = CreateUniqueId();
+            TagHelperContext context = new TagHelperContext(contextAttributes, emptyContextItems, uniqueId);
+
+            // ACT & ASSERT
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.ProcessAsync(context, output: null));
+        }
+
+        [Fact]
+        public async void AddActiveClassWhenItemSelectedAndClassAttributeNotPresent()
+        {
+            // ARRANGE
+            var activeItemValue = "AnchorText 123";
+            var sut = new ActiveAnchorTagHelper() { ActiveItem = activeItemValue };
+
+            var contextAttributes = CreateAttributeList(("href", "#"), ("active-item", activeItemValue));
+            var emptyContextItems = CreateEmptyContextItems();
+            var uniqueId = CreateUniqueId();
+            TagHelperContext context = new TagHelperContext(contextAttributes, emptyContextItems, uniqueId);
+
+            var outputAttributes = CreateAttributeList(("href", "#"));
+            var childContentFactory = CreateChildContentFactory(activeItemValue);
+            TagHelperOutput output = new TagHelperOutput("a", outputAttributes, childContentFactory);
+
+            // ACT
+            await sut.ProcessAsync(context, output);
+
+            // ASSERT
+            var classAttribute = output.Attributes.SingleOrDefault(a => a.Name == "class");
+            Assert.NotNull(classAttribute);
+
+            var classValueString = classAttribute.Value as string;
+            Assert.NotNull(classValueString);
+
+            var classes = classValueString.Split(" ");
+            Assert.Single(classes, "active");
+        }
+
+        [Fact]
+        public async void AddActiveClassWhenItemSelectedAndClassAttributePresent()
+        {
+            // ARRANGE
+            var activeItemValue = "AnchorText 123";
+            var sut = new ActiveAnchorTagHelper() { ActiveItem = activeItemValue };
+
+            var contextAttributes = CreateAttributeList(("class", "text-light"), ("href", "#"), ("active-item", activeItemValue));
+            var emptyContextItems = CreateEmptyContextItems();
+            var uniqueId = CreateUniqueId();
+            TagHelperContext context = new TagHelperContext(contextAttributes, emptyContextItems, uniqueId);
+
+            var outputAttributes = CreateAttributeList(("class", "text-light"), ("href", "#"));
+            var childContentFactory = CreateChildContentFactory(activeItemValue);
+            TagHelperOutput output = new TagHelperOutput("a", outputAttributes, childContentFactory);
+
+            // ACT
+            await sut.ProcessAsync(context, output);
+
+            // ASSERT
+            var classAttribute = output.Attributes.SingleOrDefault(a => a.Name == "class");
+            Assert.NotNull(classAttribute);
+
+            var classValueString = classAttribute.Value as string;
+            Assert.NotNull(classValueString);
+
+            var classes = classValueString.Split(" ");
+            Assert.Single(classes, "active");
+        }
+
+        [Fact]
+        public async void KeepSingleActiveClassWhenItemSelectedAndActiveClassAlreadyPresent()
+        {
+            // ARRANGE
+            var activeItemValue = "AnchorText 123";
+            var sut = new ActiveAnchorTagHelper() { ActiveItem = activeItemValue };
+
+            var contextAttributes = CreateAttributeList(("class", "text-light active"), ("href", "#"), ("active-item", activeItemValue));
+            var emptyContextItems = CreateEmptyContextItems();
+            var uniqueId = CreateUniqueId();
+            TagHelperContext context = new TagHelperContext(contextAttributes, emptyContextItems, uniqueId);
+
+            var outputAttributes = CreateAttributeList(("class", "text-light"), ("href", "#"));
+            var childContentFactory = CreateChildContentFactory(activeItemValue);
+            TagHelperOutput output = new TagHelperOutput("a", outputAttributes, childContentFactory);
+
+            // ACT
+            await sut.ProcessAsync(context, output);
+
+            // ASSERT
+            var classAttribute = output.Attributes.SingleOrDefault(a => a.Name == "class");
+            Assert.NotNull(classAttribute);
+
+            var classValueString = classAttribute.Value as string;
+            Assert.NotNull(classValueString);
+
+            var classes = classValueString.Split(" ");
+            Assert.Single(classes, "active");
+        }
+
+        [Fact]
+        public async void RemoveActiveClassWhenItemNotSelectedAndActiveClassPresent()
+        {
+            // ARRANGE
+            var activeItemValue = "AnchorText 123";
+            var sut = new ActiveAnchorTagHelper() { ActiveItem = activeItemValue };
+
+            var contextAttributes = CreateAttributeList(("class", "text-light active"), ("href", "#"), ("active-item", activeItemValue));
+            var emptyContextItems = CreateEmptyContextItems();
+            var uniqueId = CreateUniqueId();
+            TagHelperContext context = new TagHelperContext(contextAttributes, emptyContextItems, uniqueId);
+
+            var outputAttributes = CreateAttributeList(("class", "text-light"), ("href", "#"));
+            var childContentFactory = CreateChildContentFactory("Different Anchor Text 456");
+            TagHelperOutput output = new TagHelperOutput("a", outputAttributes, childContentFactory);
+
+            // ACT
+            await sut.ProcessAsync(context, output);
+
+            // ASSERT
+            var classAttribute = output.Attributes.SingleOrDefault(a => a.Name == "class");
+            Assert.NotNull(classAttribute);
+
+            var classValueString = classAttribute.Value as string;
+            Assert.NotNull(classValueString);
+
+            var classes = classValueString.Split(" ");
+            Assert.DoesNotContain("active", classes);
+        }
+
+        [Fact]
+        public async void HaveNoEffectOnClassesWhenItemNotSelectedAndActiveClassNotPresent()
+        {
+            // ARRANGE
+            var activeItemValue = "AnchorText 123";
+            var sut = new ActiveAnchorTagHelper() { ActiveItem = activeItemValue };
+
+            var contextAttributes = CreateAttributeList(("class", "text-light"), ("href", "#"), ("active-item", activeItemValue));
+            var emptyContextItems = CreateEmptyContextItems();
+            var uniqueId = CreateUniqueId();
+            TagHelperContext context = new TagHelperContext(contextAttributes, emptyContextItems, uniqueId);
+
+            var outputAttributes = CreateAttributeList(("class", "text-light"), ("href", "#"));
+            var childContentFactory = CreateChildContentFactory("Different Anchor Text 456");
+            TagHelperOutput output = new TagHelperOutput("a", outputAttributes, childContentFactory);
+
+            // ACT
+            await sut.ProcessAsync(context, output);
+
+            // ASSERT
+            var classAttribute = output.Attributes.SingleOrDefault(a => a.Name == "class");
+            Assert.NotNull(classAttribute);
+
+            var classValueString = classAttribute.Value as string;
+            Assert.NotNull(classValueString);
+
+            var classes = classValueString.Split(" ");
+            Assert.DoesNotContain("active", classes);
+        }
+
+        #endregion Test methods
+
+
+        #region Private helpers
+
+        private TagHelperAttributeList CreateAttributeList(params (string name, string value)[] attributes)
+        {
+            var attributesList = attributes.Select(a => new TagHelperAttribute(a.name, a.value)).ToList();
+            return new TagHelperAttributeList(attributesList);
+        }
+
+        private Func<bool, HtmlEncoder, Task<TagHelperContent>> CreateChildContentFactory(string childContent)
+        {
+            return (useCachedResult, htmlEncoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                tagHelperContent.SetContent(childContent);
+                return Task.FromResult<TagHelperContent>(tagHelperContent);
+            };
+        }
+
+        private IDictionary<object, object> CreateEmptyContextItems()
+        {
+            return new Dictionary<object, object>();
+        }
+
+        private string CreateUniqueId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        #endregion Private helpers
+    }
+}
---
+++ src/CramCoding/CramCoding.WebApp/TagHelpers/ActiveAnchorTagHelper.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CramCoding.WebApp.TagHelpers
+{
+    /// <summary>
+    /// Adds or removes <c>active</c> class on the target anchor tag
+    /// depending on the anchor text and the given <see cref="ActiveItem"/> parameter.
+    /// </summary>
+    /// <remarks>Adds <c>active</c> class when anchor text equals <see cref="ActiveItem"/> parameter.</remarks>
+    /// <remarks>Removes <c>active</c> class otherwise.</remarks>
+    [HtmlTargetElement("a", Attributes = ActiveItemAttributName)]
+    public class ActiveAnchorTagHelper : TagHelper
+    {
+        private const string ActiveItemAttributName = "active-item";
+
+        [HtmlAttributeName(ActiveItemAttributName)]
+        public string ActiveItem { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var anchorText = (await output.GetChildContentAsync()).GetContent();
+            if (anchorText == ActiveItem)
+            {
+                EnsureActiveClassAdded(output);
+            }
+            else
+            {
+                EnsureActiveClassRemoved(output);
+            }
+        }
+
+        private void EnsureActiveClassAdded(TagHelperOutput output)
+        {
+            var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
+
+            // Anchor tag has no "class" attribute defined yet
+            if (classAttr == null)
+            {
+                classAttr = new TagHelperAttribute("class", "active");
+                output.Attributes.Add(classAttr);
+            }
+            // Anchor tag has "class" attribute defined but its value is "null"
+            else if (classAttr.Value == null)
+            {
+                output.Attributes.SetAttribute("class", "active");
+            }
+            // Anchor tag has "class" attribute defined but it does not contain the "active" class on its list
+            else if (!classAttr.Value.ToString().Contains("active"))
+            {
+                var newClassAttrValue = classAttr.Value.ToString() + " active";
+                output.Attributes.SetAttribute("class", newClassAttrValue);
+            }
+            // Anchor tag has "class" attribute defined and it already contains the "active" class on its list
+        }
+
+        private void EnsureActiveClassRemoved(TagHelperOutput output)
+        {
+            var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
+
+            // Remove "active" class if present
+            if (classAttr != null && classAttr.Value != null && classAttr.Value.ToString().Contains("active"))
+            {
+                var classes = classAttr.Value.ToString().Split();
+                if (classes.Contains("active"))
+                {
+                    var newClassAttrValue = String.Join(" ", classes.Where(c => c != "active"));
+                    output.Attributes.SetAttribute("class", newClassAttrValue);
+                }
+            }
+        }
+    }
+}

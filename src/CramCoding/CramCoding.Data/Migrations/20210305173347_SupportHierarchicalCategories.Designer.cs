d68afa7b1ca13290bf5531945e7b4b0c67e7a477
michalantolik <michal.antolik@gmail.com>
2021-03-05 18:54:05 +0100

    Support hierarchical categories in DB
    - Category table made self-referencing
    
    References:
    
    - https://www.mikesdotnetting.com/article/255/entity-framework-recipe-hierarchical-data-management

--- src/CramCoding/CramCoding.Data/Migrations/20210305173347_SupportHierarchicalCategories.Designer.cs
+++ src/CramCoding/CramCoding.Data/Migrations/20210305173347_SupportHierarchicalCategories.Designer.cs
@@ -4,14 +4,16 @@ using CramCoding.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CramCoding.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210305173347_SupportHierarchicalCategories")]
+    partial class SupportHierarchicalCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/CramCoding/CramCoding.Domain/Entities/Category.cs
+++ src/CramCoding/CramCoding.Domain/Entities/Category.cs
@@ -5,10 +5,19 @@ namespace CramCoding.Domain.Entities
 {
     public class Category
     {
+
+        #region Constructor
+
         public Category()
         {
+            Children = new List<Category>();
             Posts = new List<Post>();
         }
+
+        #endregion
+
+
+        #region Own properties
 
         [Key]
         public int CategoryId { get; set; }
@@ -16,7 +25,17 @@ namespace CramCoding.Domain.Entities
         [Required]
         public string Name { get; set; }
 
+        #endregion Own properties
+
+
+        #region Navigation properties
+
+        public Category Parent { get; set; }
+
+        public ICollection<Category> Children { get; set; }
+
         public ICollection<Post> Posts { get; set; }
 
+        #endregion
     }
 }
